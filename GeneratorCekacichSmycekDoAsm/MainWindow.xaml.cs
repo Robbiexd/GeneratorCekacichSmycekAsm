4dbb05e5e4e63cc0f12eedc53f39b5c64779a250
Robert <48084300+Robbiexd@users.noreply.github.com>
2020-01-12 16:18:11 +0100

    Add project files.

---
+++ GeneratorCekacichSmycekDoAsm/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Model;
+
+namespace GeneratorCekacichSmycekDoAsm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Generator generator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            generator = new Generator();
+        }
+
+        private void generate_Click(object sender, RoutedEventArgs e)
+        {
+            if (TryFloat(T.Text) != -1 && TryFloat(Fosc.Text) != -1 && TryInt(Nic.Text) != -1)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    vypis(openFileDialog.FileName, generator.vypis(TryFloat(T.Text), TryFloat(Fosc.Text), TryInt(Nic.Text)));
+                    MessageBox.Show("Kód byl úspěšně vygenerován!", "Success");
+                }
+            }
+            else { MessageBox.Show("Překontrolujte si zadané hodnoty!!!", "Error"); }
+        }
+
+        float TryFloat(string num)
+        {
+
+            float number;
+            bool success = float.TryParse(num, out number);
+            if (success)
+            {
+                return number;
+            }
+            else
+            {
+                string error = ("Attempted conversion of " + num + " failed.");
+                MessageBox.Show(error, "ERROR!");
+                return -1;
+            }
+
+
+        }
+        int TryInt(string num)
+        {
+            int number;
+            bool success = Int32.TryParse(num, out number);
+            if (success)
+            {
+                return number;
+            }
+            else
+            {
+                string error = ("Attempted conversion of " + num + " failed.");
+                MessageBox.Show(error, "ERROR!");
+                return -1;
+            }
+
+
+        }
+
+        public void vypis(string path, string vystup)
+        {
+            File.WriteAllText(path, vystup);
+        }
+    }
+}
---
+++ Model/Generator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1.Model
+{
+    public class Generator
+    {
+        
+        const string tab = "                ";   
+        
+        public Generator()
+        {
+                
+        }
+
+        public string vypis(float t, float Fosc, int Nic)
+        {
+            string hex = zpracuj(t, Fosc, Nic);
+
+            //pripraveni vstupu pro vypsani na vystup
+            if(hexCount(hex) % 2 != 0)
+            {
+                hex = "0" + hex;
+            }
+
+            List<char> l1 = new List<char>();
+            List<char> l2 = new List<char>();
+            
+            int numOfLoops = 0;
+            foreach (char i in hex)
+            {   
+                if(numOfLoops % 2 == 0)
+                { l1.Add(i);  }
+                else { l2.Add(i); }
+                numOfLoops++;
+            }
+
+
+            //generace vystupu
+            string output = "";
+            string zbytek_kodu = "cekej0: ";
+            int citac = 0;
+            //int cr = 16; citac0 = registr 16
+            for (int i = 0; i < hexCount(hex); i++)
+            {
+                if (i <= l1.Count - 1 && i <= l2.Count - 1)
+                { output += "LDI citac" + i + tab + "0x" + l1[i] + l2[i] + '\n'; }
+                
+                if (i == 1) { zbytek_kodu += "?BRPL"; }
+ 
+                if (i % 2 != 0)
+                { 
+                   zbytek_kodu += tab + "DEC citac" + citac + '\n'; citac++;
+                }
+                else { zbytek_kodu += tab + "BRNE cekej0" + '\n'; }
+
+                if (hexCount(hex) == 2)
+                { break; }
+            }
+            //cr = 0;
+            return string.Format(output + zbytek_kodu);
+        }
+
+
+        //vypocitani hexa hodnoty ze vstupu
+        public string zpracuj(float t, float Fosc, int Nic)
+        {
+            float Tic;
+            float Fic;
+            float N;
+            string N_Hex;
+            Fic = (Fosc / Nic);
+            Tic = 1 / Fic;
+            N = (t / Tic);
+            int decValue = (int)(N / 3);
+            N_Hex = decValue.ToString("X");
+            //MessageBox.Show("Fic is " + Fic + "Tic is  " + Tic + "N is " + N + "N_HEx is" + N_Hex);
+            return N_Hex;
+        }
+
+        //vrati pocet cislic hexa hodnoty
+        public int hexCount(string hex)
+        {
+            int hex_count = 0;
+            foreach(char c in hex)
+            {
+                hex_count++;
+            }
+            return hex_count;
+        }
+    }
+}
